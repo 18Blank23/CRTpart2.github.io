c4c499e10c7c866907bbb10ec470f74677aed662
18Blank23 <85660344+18Blank23@users.noreply.github.com>
2021-07-22 17:23:53 +0800

    Update config.php

--- admin/php/config.php
+++ admin/php/config.php
@@ -1,9 +1,8 @@
 <?php
-  $hostname = "localhost";
-  $username = "root";
-  $password = "";
-  $dbname = "chatapp";
-
+$hostname = "remotemysql.com";
+  $username = "cix3dqBVsU";
+  $password = "nn8hdgwJTx";
+  $dbname = "cix3dqBVsU";
   $conn = mysqli_connect($hostname, $username, $password, $dbname);
   if(!$conn){
     echo "Database connection error".mysqli_connect_error();
